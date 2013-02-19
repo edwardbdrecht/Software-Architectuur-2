6c6efc4786300e7965d9e3bb6c0c89c7d5fb2abc
Edward Roozeboom <info@edwardroozeboom.com>
2013-02-19 13:16:05 +0100

    Base project files AsbtractFactory

---
+++ SoepMachine/src/soepmachine/AbstractFactory/ChickensoupFactory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soepmachine.AbstractFactory;
+
+/**
+ *
+ * @author edwardroozeboom23
+ */
+public class ChickensoupFactory {
+	
+}
---
+++ SoepMachine/src/soepmachine/AbstractProducts/AbstractPowder.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soepmachine.AbstractProducts;
+
+/**
+ *
+ * @author edwardroozeboom23
+ */
+public class AbstractPowder {
+	
+}
